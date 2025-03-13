49d343312456a2eb6732cdec50367179a116829c
KateBozhko <bozhko2906872020@gmail.com>
2025-03-13 17:57:40 +0300

    modified App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import image from './first_image.jpg';
+import { GroceryList } from './GroceryList';
 import imageTwo from './man.jpg'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='app'>
       <img src={image} width="250px" alt="shopping"/>
       <h1>Grocery List</h1>
+      <GroceryList/>
       <img src={imageTwo} width="250px" alt="man"/>
     </div>
   );
